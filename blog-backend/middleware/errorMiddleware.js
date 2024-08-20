c62925c4a3fd9fee3c54f6cf311dd7d396dcea20
Pratham Choudhary <prathamchoudhary924@gmail.com>
2024-08-20 19:53:51 +0530

    frontend setup

--- blog-backend/middleware/errorMiddleware.js
+++ blog-backend/middleware/errorMiddleware.js
@@ -6,19 +6,16 @@ const errorHandler = (err, req, res, next) => {
 
   console.log(err.stack);
 
-  // Mongoose bad ObjectId
   if (err.name === 'CastError') {
     const message = `Resource not found`;
     error = new ErrorResponse(message, 404);
   }
 
-  // Mongoose duplicate key
   if (err.code === 11000) {
     const message = 'Duplicate field value entered';
     error = new ErrorResponse(message, 400);
   }
 
-  // Mongoose validation error
   if (err.name === 'ValidationError') {
     const message = Object.values(err.errors).map(val => val.message);
     error = new ErrorResponse(message, 400);
